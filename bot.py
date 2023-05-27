f8e112c50cb4b8a3bd3d640040a9cc080fdee120
Bibhas <bd@nuiteq.com>
2023-05-28 00:46:30 +0530

    added raw query and incremental backoff

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import requests
 import re
 import os
 from dotenv import dotenv_values
+import time
 
 
 config = dotenv_values(".env")
@@ -13,6 +14,26 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+def openai_request(msg, num_try=1):
+    r = requests.post("https://api.openai.com/v1/chat/completions", json={
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": msg}]
+        }, headers={
+            'Content-Type': "application/json",
+            'Authorization': "Bearer {openai_token}".format(openai_token=config.get("OPENAI_TOKEN"))
+    })
+
+    response = r.json()
+    if 'choices' in response:
+        return response['choices'][0]['message']['content']
+    else:
+        print("Error in openai response")
+        print(response)
+        print("sleeping ", num_try, " seconds, and trying again")
+        time.sleep(num_try)
+        return openai_request(msg, num_try + 1)
+
 
 @client.event
 async def on_ready():
@@ -23,43 +44,46 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    print(message)
-    print(message.content)
+    if re.search(r"(\d) (day|night)s? in (.+)", message.content.lower()):
+        print(message)
+        print(message.content)
 
-    if re.search(r"(\d) (day|night)s? in (.+)", message.content):
-        # curl https://api.openai.com/v1/chat/completions   -H 'Content-Type: application/json'   -H 'Authorization: Bearer <foo>'   -d '{
-        #   "model": "gpt-3.5-turbo",
-        #   "messages": [{"role": "user", "content": "write a travel itinerary for wayanad for 3 nights"}]
-        # }'
         num, day_or_night, place = re.search(r"(\d) (day|night)s? in (.+)", message.content).groups()
         if len(place) > 12 or int(num) > 30:
-            await message.channel.send("lol")
+            await message.reply("lol")
             return
 
         filename = "{place}-{num}-{dorn}.txt".format(place=place, num=num, dorn=day_or_night)
         if os.path.isfile(filename):
             with open(filename, 'r') as f:
-                await message.channel.send(file=discord.File(f))
+                await message.reply(file=discord.File(f))
         else:
-            r = requests.post("https://api.openai.com/v1/chat/completions", json={
-            "model": "gpt-3.5-turbo",
-            "messages": [{"role": "user", "content": "write a travel itinerary for {place} for {num} {dorn}s".format(place=place, num=num, dorn=day_or_night)}]
-            }, headers={
-                'Content-Type': "application/json",
-                'Authorization': "Bearer {openai_token}".format(openai_token=config.get("OPENAI_TOKEN"))
-            })
-
-            response = r.json()
-            if 'choices' in response:
-                content = response['choices'][0]['message']['content']
+            content = openai_request(
+                "write a travel itinerary for {place} for {num} {dorn}s".format(
+                    place=place, num=num, dorn=day_or_night
+                )
+            )
+            if content:
                 with open(filename, 'w') as f:
                     f.write(content)
 
                 with open(filename, 'r') as f:
-                    await message.channel.send(file=discord.File(f))
+                    await message.reply(file=discord.File(f))
             else:
-                print(response)
-                await message.channel.send("could not get a response. please try again.")
+                await message.reply("could not get a response. please try again.")
+    elif message.content.lower().startswith("raw query"):
+        print(message)
+        print(message.content)
+
+        msg = message.content.replace("raw query", "").strip()
+        if msg:
+            content = openai_request(msg)
+            if content:
+                await message.reply(content)
+            else:
+                await message.reply("could not get a response. please try again.")
+        else:
+            await message.reply("what is your query?")
 
 
 client.run(config.get('DISCORD_BOT_TOKEN'))
